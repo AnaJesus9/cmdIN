9f24044f28c36a510cbd1d912ea9b8edc05fe1c4
César Silva <cesarsilva79@gmail.com>
2018-11-02 21:25:01 -0100

    added a Request class

--- src/org/academiadecodigo/bootcamp/client/Menu.java
+++ src/org/academiadecodigo/bootcamp/client/Menu.java
@@ -2,17 +2,29 @@ package org.academiadecodigo.bootcamp.client;
 
 import org.academiadecodigo.bootcamp.Prompt;
 import org.academiadecodigo.bootcamp.scanners.menu.MenuInputScanner;
+import org.academiadecodigo.bootcamp.scanners.string.PasswordInputScanner;
 import org.academiadecodigo.bootcamp.scanners.string.StringInputScanner;
+
+import java.io.*;
+import java.net.Socket;
 
 public class Menu {
 
 
-    public static final String[] LOGIN = {"Login", "Register"};
-    public static final String[] MAIN_MENU = {"Edit Profile", "Search" , "Quit"};
+    private final String[] LOGIN = {"Login", "Register"};
+    private final String[] MAIN_MENU = {"Edit Profile", "Search" , "Quit"};
     private Prompt prompt;
+    private PrintWriter output;
+    private BufferedReader input;
 
 
-    public Integer getLogin() {
+    public Menu(Socket socket) throws IOException {
+        this.prompt = new Prompt(System.in, System.out);
+        this.output = new PrintWriter(socket.getOutputStream());
+        this.input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+
+    public Integer getLogin() throws IOException{
         MenuInputScanner input = new MenuInputScanner(LOGIN);
 
         input.setMessage("Welcome to cmdIN\n What do you wanna do?");
@@ -27,15 +39,42 @@ public class Menu {
         return prompt.getUserInput(input);
     }
 
-    private void requestLogin() {
+    private void requestLogin() throws IOException {
         StringInputScanner inputUser = new StringInputScanner();
+        PasswordInputScanner inputPassword = new PasswordInputScanner();
+
+        String userName = "";
+        String password = "";
 
         inputUser.setMessage("Enter your username: ");
-        prompt.getUserInput(inputUser);
-        
+
+        while (!(validateUser(userName = prompt.getUserInput(inputUser),
+                password = prompt.getUserInput(inputPassword)))) {
+
+            System.out.println("Invalid username or password.");
+        }
+        getMainMenu(userName);
     }
 
+
+
     private void requestRegisterData() {
+
+    }
+
+    private boolean validateUser(String userName, String password) throws IOException{
+        String login = "user: user, password: password\n";
+
+        output.println(login);
+        return (input.readLine() == "true");
+    }
+
+    private void getMainMenu(String userName) {
+        MenuInputScanner inputOption = new MenuInputScanner(MAIN_MENU);
+        String name = request.getName;
+
+
+        inputOption.setMessage("### Welcome " + name + ", your on the Main Menu ###");
 
     }
 
---
+++ src/org/academiadecodigo/bootcamp/client/Request.java
@@ -0,0 +1,10 @@
+package org.academiadecodigo.bootcamp.client;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
+public class Request {
+
+    private PrintWriter output;
+    private BufferedReader input;
+}
