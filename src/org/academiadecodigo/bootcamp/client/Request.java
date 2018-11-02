57598474b66fe0c548c7b424f27b7fcc146c629d
César Silva <cesarsilva79@gmail.com>
2018-11-02 21:42:39 -0100

    login request in request class done

--- src/org/academiadecodigo/bootcamp/client/Request.java
+++ src/org/academiadecodigo/bootcamp/client/Request.java
@@ -1,10 +1,25 @@
 package org.academiadecodigo.bootcamp.client;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.Socket;
 
 public class Request {
 
     private PrintWriter output;
     private BufferedReader input;
+    
+
+    public Request(Socket socket) throws IOException {
+        this.output = new PrintWriter(socket.getOutputStream());
+        this.input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+
+    public boolean login(String userName, String password) throws IOException {
+        output.println(userName + " " + password + "\n");
+
+        return (input.readLine() == "logOn");
+    }
 }
