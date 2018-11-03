65e5c01b0bd0bbace12e40a4ae19639879e47fee
César Silva <cesarsilva79@gmail.com>
2018-11-03 12:21:56 -0100

    changes made to Client class

--- src/org/academiadecodigo/bootcamp/client/Client.java
+++ src/org/academiadecodigo/bootcamp/client/Client.java
@@ -1,6 +1,8 @@
 package org.academiadecodigo.bootcamp.client;
 
+import java.io.IOException;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Client {
 
@@ -9,11 +11,45 @@ public class Client {
 
     public static void main(String[] args) {
 
+        if (args.length != 2) {
+            System.err.println("Usage: java -jar cmdIN <host> <port>");
+        }
+
+        Client client = new Client();
+
+        try {
+            client.establishConnection(args[0], Integer.parseInt(args[1]));
+
+        } catch (NumberFormatException e) {
+            System.err.println("Invalid port number " + args[1]);
+
+        } catch (UnknownHostException e) {
+            System.err.println("Host Unknown" + e.getMessage() + " :closing connection...");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            closeSocket();
+        }
+
+
     }
 
-    private void establishConnection(String  serverAdress, int serverPort) {
-        socket = new Socket(serverAdress, serverPort);
+    private void establishConnection(String  serverAddress, int serverPort) throws IOException {
+        socket = new Socket(serverAddress, serverPort);
 
         System.out.println("Connected to: " + socket.getReceiveBufferSize());
+    }
+
+    private void closeSocket() {
+
+        try {
+            if (socket != null) {
+                System.out.println("Closing the socket");
+                socket.close();
+            }
+        } catch (IOException e) {
+                System.out.println("Error closing connection: " + e.getMessage());
+            }
+        }
     }
 }
